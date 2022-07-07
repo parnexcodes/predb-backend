5c0ba740da32b02a4a5b26931f56d70f24255645
parnexcodes <parnexcodes@gmail.com>
2022-07-08 00:05:47 +0530

    fix formatting and add root endpoint

--- index.js
+++ index.js
@@ -1,79 +1,100 @@
-var irc = require('irc');
-var ircf = require('irc-formatting');
+var irc = require("irc");
+var ircf = require("irc-formatting");
 
-const fastify = require('fastify')({ logger: true })
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const fastify = require("fastify")({ logger: true });
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-var client = new irc.Client('irc.corrupt-net.org', 'parnexsurf', {
-    channels: ['#Pre'],
-    port: 6667
+var client = new irc.Client("irc.corrupt-net.org", "parnexsurf", {
+  channels: ["#Pre"],
+  port: 6667,
 });
 
-client.addListener('message', async function (from, to, message) {
-    let msg = ircf.parse(message)
-    let pre_category = msg[2].text
-    let pre_title = msg[3].text.replace(']', '').trim()
-    let pre_group = msg[3].text.split('-').pop()
+client.addListener("message", async function (from, to, message) {
+  let msg = ircf.parse(message);
+  let pre_category = msg[2].text;
+  let pre_title = msg[3].text.replace("]", "").trim();
+  let pre_group = msg[3].text.split("-").pop();
 
-    if (pre_category.includes('dupe')) {
-        pre_title = pre_category
-        pre_category = 'PRE'
-        pre_group = 'nuke'
-    }
+  if (pre_category.includes("dupe")) {
+    pre_title = pre_category;
+    pre_category = "PRE";
+    pre_group = "nuke";
+  }
 
-    if (pre_title.toLowerCase().includes('web.h264') && pre_category.includes('PRE')) {
-        pre_category = 'X264'
-    }
+  if (
+    pre_title.toLowerCase().includes("web.h264") &&
+    pre_category.includes("PRE")
+  ) {
+    pre_category = "X264";
+  }
 
-    if (pre_title.toLowerCase().includes('web.h265') && pre_category.includes('PRE')) {
-        pre_category = 'X265'
-    }
+  if (
+    pre_title.toLowerCase().includes("web.h265") &&
+    pre_category.includes("PRE")
+  ) {
+    pre_category = "X265";
+  }
 
-    if (pre_title.includes('COMPLETE')) {
-        pre_category = 'BLURAY'
-    }
+  if (pre_title.includes("COMPLETE")) {
+    pre_category = "BLURAY";
+  }
 
-    const postPre = await prisma.pre.create({
-        data: {
-            preCategory: pre_category,
-            preTitle: pre_title,
-            preGroup: pre_group
-        }
-    })
-    console.log(postPre)
+  const postPre = await prisma.pre.create({
+    data: {
+      preCategory: pre_category,
+      preTitle: pre_title,
+      preGroup: pre_group,
+    },
+  });
+  // console.log(postPre)
 });
 
 // Declare a route
-fastify.get('/api/pre', async (request, reply) => {
-    let { order, page } = request.query
-    if (!page) {
-        page = 0
-    }
-    if (!order) {
-        order = 'desc'
-    }
-    if (order != 'asc' && order != 'desc') {
-        order = 'desc'
-    }
-    
-    const getPre = await prisma.pre.findMany({
-        skip: page * 20,
-        take: 20,
-        orderBy: {
-            id: order
-        }
-    })
-    return { result: getPre }
-  })
+fastify.get("/", { prefix: "/api" }, async (request, reply) => {
+  return {
+    info: "Welcome to predb API.",
+    endpoints: [
+      {
+        pre: "/api/pre",
+      },
+    ],
+  };
+});
+
+fastify.get("/*", async (request, reply) => {
+  return { error: "Undefined Endpoint" };
+});
+
+fastify.get("/api/pre", async (request, reply) => {
+  let { order, page } = request.query;
+  if (!page) {
+    page = 0;
+  }
+  if (!order) {
+    order = "desc";
+  }
+  if (order != "asc" && order != "desc") {
+    order = "desc";
+  }
+
+  const getPre = await prisma.pre.findMany({
+    skip: page * 20,
+    take: 20,
+    orderBy: {
+      id: order,
+    },
+  });
+  return { result: getPre };
+});
 
 // Run the server!
 const start = async () => {
-    try {
-      await fastify.listen({ port: 8080, host: '0.0.0.0' })
-    } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
+  try {
+    await fastify.listen({ port: 8080, host: "0.0.0.0" });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
   }
-  start()
+};
+start();
